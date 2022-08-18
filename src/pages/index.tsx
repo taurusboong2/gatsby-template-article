b7aca36eb7e61e45ea022b770b2a4851eeeecaa3
boong <taurusboong2@gmail.com>
2022-08-18 22:16:49 +0900

    Configure eslint

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,14 @@
-import * as React from "react"
-import type { HeadFC } from "gatsby"
+import * as React from 'react';
+import type { HeadFC } from 'gatsby';
 
-export const Head: HeadFC = () => <title>Home Page</title>
+export const Head: HeadFC = () => <title>Home Page</title>;
 
 const IndexPage = () => {
   return (
     <main>
-      <h1>Index page</h1>
+      <h1>Index page boong</h1>
     </main>
-  )
-}
+  );
+};
 
-export default IndexPage
-
+export default IndexPage;
