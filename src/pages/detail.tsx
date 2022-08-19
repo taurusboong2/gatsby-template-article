7a37eda397b8106258c7a6d1d781195e4a41062c
boong <taurusboong2@gmail.com>
2022-08-19 17:59:05 +0900

    Feat : add detail page

---
+++ src/pages/detail.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Footer from '../components/Footer';
+
+const Detail = () => {
+  return (
+    <>
+      <h1>detail page</h1>
+      <Footer />
+    </>
+  );
+};
+
+export default Detail;
