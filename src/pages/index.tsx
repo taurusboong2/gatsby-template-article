6b82453ed3385c16f256130902700ba0d795935e
boong <taurusboong2@gmail.com>
2022-08-20 23:59:27 +0900

    Feat : linkage data through graphql

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,43 @@
 import React from 'react';
+import { graphql, PageProps } from 'gatsby';
+import { Article } from '../types/articles';
 import ArticleList from '../components/ArticleList';
-import Pagination from '../components/Pagination';
 import Footer from '../components/Footer';
 
-const Home = () => {
+type Props = PageProps<{ articles: { nodes: Article[] } }>;
+
+const Home = (props: Props) => {
+  const articles = props.data.articles.nodes;
+
   return (
     <>
-      <ArticleList />
-      <Pagination />
+      <ArticleList articles={articles} />
       <Footer />
     </>
   );
 };
 
 export default Home;
+
+export const pageQuery = graphql`
+  query {
+    articles: allMarkdownRemark {
+      nodes {
+        html
+        frontmatter {
+          date(formatString: "MMMM DD, YYYY")
+          slug
+          title
+          tags
+          description
+          category
+          writer
+          thumbnail {
+            publicURL
+            name
+          }
+        }
+      }
+    }
+  }
+`;
